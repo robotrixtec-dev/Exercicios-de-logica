6edd864937ad8c82f2d5ec419b9879ac9a618189
robotrixtech <robotrix.tec@gmail.com>
2026-08-07 16:14:37 -0300

    Add input and comparison logic for total calculation

--- resolucao_exercicio_03.vb
+++ resolucao_exercicio_03.vb
@@ -1,1 +1,18 @@
+TextWindow.Write("Digite o valor A: ")
+valorA = TextWindow.ReadNumber()
 
+TextWindow.Write("Digite o valor B: ")
+valorB = TextWindow.ReadNumber()
+
+total = valorA + valorB
+
+If total > 10 Then
+  mensagem = "O total é maior que 10"
+ElseIf total < 10 Then
+  mensagem = "O total é menor que 10"
+Else
+  mensagem = "O total é igual a 10"
+EndIf
+
+TextWindow.WriteLine("Total calculado: " + total)
+TextWindow.WriteLine(mensagem)
